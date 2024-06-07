98e945c92e99582e7fd25ef02081ba07dc576e13
Massinissa <massinissaaittaleb2003@gmail.com>
2024-06-07 21:54:45 +0100

    v1.0

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,69 +1,147 @@
-import ChatBubble from "./Modules/ChatBubble"
-import {useState} from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import ChatBubble from './Modules/ChatBubble';
+import logo from "./assets/icons8-ai-48.png"
 
 function App() {
-  const API_Key = "sk-proj-y2eH7MeDkImwdwti4dhpT3BlbkFJLTSAVZdMgSNfBLqiW9Gq"
-  const [message,setMessage] = useState();
+  const API_Key = 'pk-hBxtJAbYuydLCsSElXlBgIviKztYYcMULPbFYgCHRzKdKgBf';
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [thinking, setThinking] = useState(false);
 
-  const handelSubmit = async (e)=>{
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages, thinking]);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const newMessage = {
-      role:"user",
-      content:message
-    } 
+
+    setThinking(true);
 
     const apiReq = {
-      "model":"gpt-3.5-turbo",
-      "messages": [
+      model: 'gpt-3.5-turbo',
+      messages: [
         {
-          "role": "system",
-          "content": "You are a helpful assistant."
-        },newMessage
+          role: 'system',
+          content: 'You are a helpful assistant.'
+        },
+        ...messages,
+        {
+          role: 'user',
+          content: message
+        }
       ]
-    }
+    };
 
-    await fetch("https://api.openai.com/v1/chat/completions",{
-      method:"POST",
-      headers:{
-        "Authorization": "Bearer "+API_Key,
-        "Content-Type" : "application/json"
+    setMessages((prevMsg) => [
+      ...prevMsg,
+      {
+        role: 'user',
+        content: message
+      }
+    ]);
+
+    setMessage('');
+
+    await fetch('https://api.pawan.krd/pai-001-light/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + API_Key,
+        'Content-Type': 'application/json'
       },
-      body:JSON.stringify(apiReq)
-    }).then(data=>console.log(data))
-  }
-  
+      body: JSON.stringify(apiReq)
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        setThinking(false);
+        setMessages((prevMsg) => [...prevMsg, data.choices[0].message]);
+      });
+  };
+
   return (
     <>
-      <div className="header" style={{display:"flex",flexDirection:"column",gap:"5px",alignItems:"center",margin:"5vh 0 5vh 0"}} >
-        <h3 style={{margin:"0px"}} >Welcome to AI ask</h3>
-        <h6 style={{margin:"0px"}} >powored by GPT 3.5</h6>
+      <div
+        className="header"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '5px',
+          alignItems: 'center',
+          margin: '5vh 0 5vh 0'
+        }}
+      >
+        <img style={{width:"48px"}} src={logo} alt="logo" />
+        <h3 style={{ margin: '0px' }}>Welcome to AI ask</h3>
+        <h6 style={{ margin: '0px' }}>powered by GPT 3.5</h6>
       </div>
-      <div className="messages">
-        <ChatBubble who="AI" />
-        <ChatBubble who="user" />
-        <span style={{margin:"10px"}} >AI is thinking . . .</span>
+      <div className="messages" style={{ marginBottom: '15%' }}>
+        {messages.length==0 && (
+          <div className="container" style={{display:"flex",justifyContent:"center",marginTop:"20vh"}}>
+            <div className="content">
+              <span style={{fontWeight:"500"}} >ask me any thing I have answers for every thing </span>
+            </div>
+          </div>
+        )}
+        {messages &&
+          messages.map((msg, index) => (
+            <ChatBubble key={index} what={msg.content} who={msg.role} />
+          ))}
+        {thinking && <span style={{ margin: '10px' }}>AI is thinking . . .</span>}
+        <div ref={messagesEndRef} />
       </div>
-      
-      <form onSubmit={handelSubmit} style={{marginLeft:"10vw",position:"fixed",bottom:"5%",border:"1px solid grey",padding:"10px",borderRadius:"20px",width:"80vw"}} >
-          <input type="text"
-                onChange={(e)=>setMessage(e.target.value)}
-                onFocus={(e) => e.target.style = 'all: unset; width: 70vw; padding-left: 10px '}
-                style={{
-                appearance: 'none',
-                WebkitAppearance: 'none',
-                MozAppearance: 'none',
-                border: 'none',
-                background: 'none',
-                color: 'inherit',
-                font: 'inherit',
-                cursor: 'pointer',
-                width:"70vw",
-                paddingLeft:"10px"
-            }}/>
-          <input type="submit" value="ask" style={{width:"8vw",height:"30px",borderRadius:"20px",border:"1px solid grey",cursor:"pointer"}} />
+
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          backgroundColor: '#242424',
+          marginLeft: '10vw',
+          position: 'fixed',
+          bottom: '5%',
+          border: '1px solid grey',
+          padding: '10px',
+          borderRadius: '20px',
+          width: '80vw'
+        }}
+      >
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          onFocus={(e) => (e.target.style = 'all: unset; width: 70vw; padding-left: 10px ')}
+          style={{
+            appearance: 'none',
+            WebkitAppearance: 'none',
+            MozAppearance: 'none',
+            border: 'none',
+            background: 'none',
+            color: 'inherit',
+            font: 'inherit',
+            cursor: 'pointer',
+            width: '70vw',
+            paddingLeft: '10px'
+          }}
+        />
+        <input
+          type="submit"
+          value="ask"
+          style={{
+            width: '8vw',
+            height: '30px',
+            borderRadius: '20px',
+            border: '1px solid grey',
+            cursor: 'pointer'
+          }}
+        />
       </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- app/src/Modules/ChatBubble.jsx
+++ app/src/Modules/ChatBubble.jsx
@@ -1,15 +1,14 @@
 
-function ChatBubble(who){
-
+function ChatBubble({who,what}){
     return(
-        <div  style={who.who=="user"?{display:"flex",justifyContent:"flex-end"}:{}} >
+        <div  style={who=="user"?{display:"flex",justifyContent:"flex-end"}:{}} >
             
-            <div className="bubble" style={who.who=="AI"?{width:"fit-content",padding:"10px",position:"relative" ,margin:"10px",backgroundColor:"grey",borderRadius:"20px"}:{width:"fit-content",padding:"10px ",position:"relative",right:"" ,margin:"10px",backgroundColor:"#7d95ad",borderRadius:"20px"}} >
+            <div className="bubble" style={who=="assistant"?{width:"fit-content",padding:"10px",position:"relative" ,margin:"10px",backgroundColor:"grey",borderRadius:"20px"}:{width:"fit-content",padding:"10px ",position:"relative",right:"" ,margin:"10px",backgroundColor:"#7d95ad",borderRadius:"20px"}} >
 
-                <span style={{color:"black",fontWeight:"600"}} >ChatGPT</span>
+                <span style={{color:"black",fontWeight:"600"}} > {who=="user"? "You" :"AI"} </span>
                 
                 <div style={{marginTop:"10px"}} >
-                    hello there 
+                    {what} 
                 </div>
             
             </div>
